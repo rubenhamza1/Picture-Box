490e33381904abb423bb6e90530cc40b23a8e3ec
rubenhamza1 <90246098+rubenhamza1@users.noreply.github.com>
2022-01-11 11:08:27 +0100

    Add files via upload

---
+++ PictureBox/PictureBox/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace PictureBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPrikazi = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnSakrij = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnPrikazi
+            // 
+            this.btnPrikazi.Location = new System.Drawing.Point(27, 459);
+            this.btnPrikazi.Name = "btnPrikazi";
+            this.btnPrikazi.Size = new System.Drawing.Size(99, 41);
+            this.btnPrikazi.TabIndex = 0;
+            this.btnPrikazi.Text = "Prikaži sliku!";
+            this.btnPrikazi.UseVisualStyleBackColor = true;
+            this.btnPrikazi.Click += new System.EventHandler(this.btnPrikazi_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::PictureBox.Properties.Resources.scale;
+            this.pictureBox1.Location = new System.Drawing.Point(-5, 55);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(435, 366);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // btnSakrij
+            // 
+            this.btnSakrij.Location = new System.Drawing.Point(282, 459);
+            this.btnSakrij.Name = "btnSakrij";
+            this.btnSakrij.Size = new System.Drawing.Size(104, 41);
+            this.btnSakrij.TabIndex = 2;
+            this.btnSakrij.Text = "Sakrij sliku!";
+            this.btnSakrij.UseVisualStyleBackColor = true;
+            this.btnSakrij.Click += new System.EventHandler(this.btnSakrij_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 526);
+            this.Controls.Add(this.btnSakrij);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnPrikazi);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPrikazi;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnSakrij;
+    }
+}
+
